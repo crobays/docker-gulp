8a056dd9be9e193b0a294d744f0fb74272360366
maikel <maikel@mediamoose.nl>
2015-04-20 15:31:25 +0000

    remove creation of default folders

--- scripts/gulp-config.sh
+++ scripts/gulp-config.sh
@@ -1,15 +1,15 @@
 #!/bin/bash
 #mkdir -p "/project/$BASE_DIR/bower_components"
 #mkdir -p "/project/$BASE_DIR/fonts"
-mkdir -p "/project/$BASE_DIR/$IMAGES_DIR"
+#mkdir -p "/project/$BASE_DIR/$IMAGES_DIR"
 #mkdir -p "/project/$BASE_DIR/pictures"
 
-mkdir -p "/project/$BASE_DIR/$STYLES_DIR"
+#mkdir -p "/project/$BASE_DIR/$STYLES_DIR"
 # if [ ! -f "/project/$BASE_DIR/$STYLES_DIR/style.sass" ];then
 # 	echo -e "body\n\tbackground-color: #eee" > "/project/$BASE_DIR/$STYLES_DIR/style.sass"
 # fi
 
-mkdir -p "/project/$BASE_DIR/$SCRIPTS_DIR"
+#mkdir -p "/project/$BASE_DIR/$SCRIPTS_DIR"
 # if [ ! -f "/project/$BASE_DIR/$SCRIPTS_DIR/script.js" ];then
 # 	echo -e "console.log(\"hello world\");" > "/project/$BASE_DIR/$SCRIPTS_DIR/script.js"
 # fi
